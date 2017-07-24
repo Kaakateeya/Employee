622e9fe79ca58031b5db91053610238f9f3da0c8
lakshmidevi <lakshmidevi407@gmail.com>
2017-07-24 11:09:27 +0530

    24_07_2017_checkin

--- app/marketingticketverification/marketingticketverificationservice.js
+++ app/marketingticketverification/marketingticketverificationservice.js
@@ -6,6 +6,14 @@
             return {
                 marketingverificationticketsubmit: function(obj) {
                     return http.post(app.apiroot + 'EmployeeReportPage/authorizationpaymentamoutReport', obj);
+                },
+                getMyprofilebind: function(flag, ID) {
+                    return http.get(app.apiroot + 'EmployeeReportPage/getMyProfileBindings', {
+                        params: {
+                            flag: flag,
+                            ID: ID,
+                        }
+                    });
                 }
             };
         }]);
