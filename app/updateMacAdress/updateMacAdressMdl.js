556d5a3ea08a5ee0df9ebc3a89cb0d23ddb1f42d
lakshmidevi <lakshmidevi407@gmail.com>
2017-08-12 18:25:44 +0530

    11_08_2017_Search results

--- app/updateMacAdress/updateMacAdressMdl.js
+++ app/updateMacAdress/updateMacAdressMdl.js
@@ -17,7 +17,9 @@
                  { text: 'SNO', key: 'sno', type: 'label' },
                  { text: 'Branch', key: 'BranchName', type: 'label' },
                  { text: 'IP Adress', key: 'MacAddressName', type: 'textbox', templateUrl: model.ipaddr, model: 'txtMac' },
+                 { text: 'IP Adress2', key: 'MacAddressName_1', type: 'textbox', templateUrl: model.ipaddr1, model: 'txtMacnew' },
                  { text: '', key: '', type: 'customlink', templateUrl: model.optionTemplate, method: model.actionlink }
+                 //  { text: '', key: '', type: 'customlink', templateUrl: model.optionTemplate1, method: model.actionlink }
              ];
              model.gridBind();
              return model;
@@ -36,32 +38,33 @@
                  _.map(model.sdata, function(item, index) {
                      item.sno = index + 1;
                      item.optionType = 'label';
+                     item.optionType1 = 'label2';
                      model['txtMac' + item.sno] = item.MacAddressName;
+                     model['txtMacnew' + item.sno] = item.MacAddressName_1;
                  });
              });
 
          };
-
-
-
-
          model.ipaddr = function(row) {
              var str = row.optionType === 'label' ? '<label>' + row.MacAddressName + '</label>' : '<input  type="text" ng-model="row.MacAddressName" class="form-control">';
              return str;
          };
-
+         model.ipaddr1 = function(row) {
+             var str = row.optionType1 === 'label1' ? '<label>' + row.MacAddressName_1 + '</label>' : '<input  type="text" ng-model="row.MacAddressName_1" class="form-control">';
+             return str;
+         };
          model.optionTemplate = function(row) {
              var lblname = row.optionType === 'label' ? 'Update' : 'save';
              var str = '<a>' + lblname + '</a>';
              return str;
          };
-
          model.actionlink = function(row) {
              if (model.Admin === '1') {
                  if (row.optionType === 'input') {
                      var ipaddr = model['txtMac' + row.sno];
+                     var ipaddr1 = model['txtMacnew' + row.sno] !== "" && model['txtMacnew' + row.sno] !== null && model['txtMacnew' + row.sno] !== undefined ? model['txtMacnew' + row.sno] : "";
                      if (ipaddr) {
-                         updateMacAdressService.UpdatemacIps(ipaddr, row.BranchID).then(function(response) {
+                         updateMacAdressService.UpdatemacIps(ipaddr, ipaddr1, row.BranchID).then(function(response) {
                              console.log(response);
                              if (response.data && parseInt(response.data) === 1) {
                                  alertss.timeoutoldalerts(model.scope, 'alert-success', 'IP Address Updated Succesfully', 4500);
