cd60695bf65ebbd1d7b9256df0eaa12125138ea0
umakusumuru <kusumuruuma@gmail.com>
2017-08-16 18:37:05 +0530

    new settle page

--- index.js
+++ index.js
@@ -103,7 +103,8 @@ app.config(['$stateProvider', '$urlRouterProvider', '$locationProvider', '$ocLaz
             { routeName: 'sucessStories', name: 'base.sucessStories', url: '/sucessStoriespage', isloginrequired: true },
             { routeName: 'EmployeePaymentInsertnew', name: 'base.EmployeePaymentInsertnew', url: '/EmployeePaymentInsertsnew/:ProfileID/:status/:paymentID/:histryid', isloginrequired: true },
             { routeName: 'empTracking', name: 'base.empTracking', url: '/empTrackingPage', isloginrequired: true },
-            { routeName: 'dashboardAdminReport', name: 'base.dashboardAdminReport', url: '/dashboardAdminReports', isloginrequired: true }
+            { routeName: 'dashboardAdminReport', name: 'base.dashboardAdminReport', url: '/dashboardAdminReports', isloginrequired: true },
+            { routeName: 'viewUploadSettledProfiles', name: 'base.viewUploadSettledProfiles', url: '/viewUploadSettledProfilesPage', isloginrequired: true },
 
 
         ];
