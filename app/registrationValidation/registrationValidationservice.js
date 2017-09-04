3b1a878d8f10a1b3aec62a8553e8eec67cc7beba
lakshmidevi <lakshmidevi407@gmail.com>
2017-09-04 13:44:36 +0530

    04_09_2017_183buildregistrationvalidATIONBUILD

--- app/registrationValidation/registrationValidationservice.js
+++ app/registrationValidation/registrationValidationservice.js
@@ -9,7 +9,11 @@
     function factory(http) {
         return {
             getRegSearchProfile: function(obj) {
-                return http.post(app.apiroot + 'EmployeeReportPage/RegistrationValidation', obj);
+                if (parseInt(obj.intflag) === 1) {
+                    return http.post(app.apiroot + 'EmployeeReportPage/RegistrationValidation', obj);
+                } else {
+                    return http.post(app.apiroot + 'EmployeeReportPage/RegistrationValidationtable', obj);
+                }
             },
             regValiplayBtn: function(pid) {
                 return http.get(app.apiroot + 'EmployeeReportPage/getRegistrationValidation_Playbutton', { params: { Profileid: pid } });
