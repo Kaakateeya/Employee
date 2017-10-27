cea3f248cdd5ac87d48d54a65471f2560b5c6813
lakshmidevi <lakshmidevi407@gmail.com>
2017-10-27 13:02:56 +0530

    27_10_2017_Matchfollowupchanges

---
+++ app/matchfollowupReport/matchfollowupReportMdl.js
@@ -0,0 +1,17 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('Kaakateeya')
+        .factory('matchfollowupReportModel', factory);
+
+    factory.$inject = ['matchfollowupReportService'];
+
+    function factory(matchfollowupReportService) {
+
+        var model = {};
+
+        return model;
+
+    }
+})();
