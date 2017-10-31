c972a020cc042ade5063e1522714af071ca3d1df
lakshmidevi <lakshmidevi407@gmail.com>
2017-10-31 16:37:18 +0530

    31_10_2017_removingcustomerApi

--- app/editPartnerpreference/editPartnerpreferenceservice.js
+++ app/editPartnerpreference/editPartnerpreferenceservice.js
@@ -4,13 +4,13 @@
     function factory(http) {
         return {
             getPartnerPreferenceData: function(obj) {
-                return http.get(app.apipathold + 'CustomerPersonal/getCustomerpartnerpreferencesDetailsDisplay', { params: { CustID: obj } });
+                return http.get(app.apiroot + 'CustomerPersonal/getCustomerpartnerpreferencesDetailsDisplay', { params: { CustID: obj } });
             },
             submitPartnerPrefData: function(obj1) {
-                return http.post(app.apipathold + 'CustomerPersonalUpdate/CustomerPartnerPreferencesUpdatedetails', JSON.stringify(obj1));
+                return http.post(app.apiroot + 'CustomerPersonalUpdate/CustomerPartnerPreferencesUpdatedetails', JSON.stringify(obj1));
             },
             submitPartnerDescData: function(obj) {
-                return http.get(app.apipathold + 'CustomerPersonal/getPartnerpreference_DiscribeYourPartner', { params: obj });
+                return http.get(app.apiroot + 'CustomerPersonal/getPartnerpreference_DiscribeYourPartner', { params: obj });
             }
         };
     }
